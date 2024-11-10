5478d59a1458871517327005ef1b49e9b19c8437
Kosiso Igbokwe <48086838+igbokwe-kosi@users.noreply.github.com>
2024-11-09 22:24:49 -0700

    added the styles to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,17 @@
+import { NavLink } from "react-router-dom";
+import styles from "./NavBar.module.css";
+import Logo from "./Logo";
+
 function NavBar() {
-  return <div>navbar</div>;
+  return (
+    <nav className={styles.nav}>
+      <Logo />
+
+      <NavLink className={styles.btn} to="/ask-juris">
+        Ask Juris
+      </NavLink>
+    </nav>
+  );
 }
 
 export default NavBar;
